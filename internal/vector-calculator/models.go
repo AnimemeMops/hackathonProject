bebeca7ded4a3c432ed91437ae039163f0852a3d
Nizom98 <nizomjon98@mail.ru>
2022-12-03 16:36:57 +0300

    refactor method

--- internal/vector-calculator/models.go
+++ internal/vector-calculator/models.go
@@ -1,5 +1,13 @@
 package vectorcalculator
 
-type VC struct {
+import "image"
 
+type Img interface {
+	Path() string
+	Img() image.Image
+}
+
+type IV struct {
+	Path   string
+	Vector []float64
 }
--- internal/vector-calculator/vector_calc.go
+++ internal/vector-calculator/vector_calc.go
@@ -1,25 +1,18 @@
 package vectorcalculator
 
-import (
-	"image"
-)
-
 const (
 	flot64_255 = float64(255)
 )
 
-func NewManager() *VC {
-	return &VC{}
-}
-
-func (vc *VC) RGBAVector(img image.Image) []float64 {
-	bounds := img.Bounds()
+// RGBAVector ...
+func RGBAVector(img Img) *IV {
+	bounds := img.Img().Bounds()
     width, height := bounds.Max.X, bounds.Max.Y
 	rgba := make([]float64, 0, width*height*4)
 
     for y := 0; y < height; y++ {
         for x := 0; x < width; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
+			r, g, b, a := img.Img().At(x, y).RGBA()
 			rgba = append(rgba, 
 				float64(r)/flot64_255,
 				float64(g)/flot64_255,
@@ -29,5 +22,8 @@ func (vc *VC) RGBAVector(img image.Image) []float64 {
         }
     }
 	
-	return rgba
+	return &IV{
+		Path: img.Path(),
+		Vector: rgba,
+	}
 }
