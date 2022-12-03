524a0205a72e3843055cc24ecd262e8b58665e29
animememops <animememops99@gmail.com>
2022-12-03 16:40:21 +0300

    Сделал новый тип для работы с изображениями

--- internal/file-reader/reader.go
+++ internal/file-reader/reader.go
@@ -16,8 +16,28 @@ type FileReader struct {
 func NewFileReader() *FileReader {
 	return &FileReader{}
 }
-func (fr *FileReader) ReadBmpImages(paths ...string) []image.Image {
-	images := make([]image.Image, 0, len(paths))
+
+type BmpImage struct {
+	path string
+	img  image.Image
+}
+
+func (bi *BmpImage) Path() string {
+	return bi.path
+}
+
+func (bi *BmpImage) Img() image.Image {
+	return bi.img
+}
+
+func newBmpImage(path string, img image.Image) *BmpImage {
+	return &BmpImage{
+		path: path,
+		img:  img,
+	}
+}
+func (fr *FileReader) ReadBmpImages(paths ...string) []*BmpImage {
+	images := make([]*BmpImage, 0, len(paths))
 	for i := range paths {
 		img, err := fr.ReadBmpImage(paths[i])
 		if err != nil {
@@ -27,12 +47,13 @@ func (fr *FileReader) ReadBmpImages(paths ...string) []image.Image {
 		images = append(images, img)
 	}
 	return images
+
 }
-func (fr *FileReader) ReadBmpImage(path string) (image.Image, error) {
+func (fr *FileReader) ReadBmpImage(path string) (*BmpImage, error) {
 	buff, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 	image, err := bmp.Decode(bytes.NewReader(buff))
-	return image, err
+	return newBmpImage(path, image), err
 }
